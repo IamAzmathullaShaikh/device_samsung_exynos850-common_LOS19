beefc07ce6f10270d3ca037c3909ab0ceda7e577
Robert Shih <robertshih@google.com>
2022-02-15 17:07:30 +0000

    exynos850-common: enable AIDL DRM HALs
    Bug: 219538389
    Test: atest GtsMediaTestCases
    Change-Id: I431554dcbef014f8235f048ee062a218a2131f9c

--- common.mk
+++ common.mk
@@ -71,7 +71,7 @@ PRODUCT_PACKAGES += \
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4.vendor:32 \
-    android.hardware.drm@1.4-service.clearkey
+    android.hardware.drm-service.clearkey
 
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
