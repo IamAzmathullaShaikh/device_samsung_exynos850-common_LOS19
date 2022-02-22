384e45d3ae5b6cf954253da3bdf8e41cd0260d65
Tim Zimmermann <tim@linux4.de>
2022-02-22 15:38:03 +0100

    exynos850-common: Switch to Samsung USB 1.3 HAL
    * Also remove lineage trust HAL as it is not required with usb HAL v1.3
    
    Change-Id: I5a7a39e0b731606470b05021bb275e8caf640953

--- common.mk
+++ common.mk
@@ -263,13 +263,9 @@ PRODUCT_SOONG_NAMESPACES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.samsung
 
-# Trust HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
-
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.1-service.typec
+    android.hardware.usb@1.3-service.samsung
 
 # Vendor service manager
 PRODUCT_PACKAGES += \
