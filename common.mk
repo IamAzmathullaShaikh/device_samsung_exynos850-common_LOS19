dd54ab9bf1b7d7efa8cb6560392b7a12bed04b3d
A2L5E0X1 <alexed25@protonmail.com>
2022-09-30 20:48:54 +0200

    exynos850-common: drop touch hal
    * since OneUI 4 update the touch hal causes bootloops on some specific models which no longer support glove mode for whatever reason.
    
    Change-Id: I1d2ca36efcbe965b5738ca911ddf0227ac9eb745

--- common.mk
+++ common.mk
@@ -264,10 +264,6 @@ PRODUCT_SOONG_NAMESPACES += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0.vendor
 
-# Touch
-PRODUCT_PACKAGES += \
-    vendor.lineage.touch@1.0-service.samsung
-
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service.samsung
